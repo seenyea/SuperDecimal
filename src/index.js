8bfc167f68a92ef974aaf69dddb65d625e377931
seenyea <seenyea@163.com>
2021-08-11 15:55:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -35,8 +35,8 @@ const IsNumeric = str => {
  * @returns {string} - 保留小数位数的除数
  */
 const decimalPlace = (n) => {
-   const divisor = BigInt('1'.padEnd(n, '0'));
-   return divisor;
+  const divisor = BigInt('1'.padEnd(n, '0'));
+  return divisor;
 }
 
 /**
@@ -46,17 +46,17 @@ const decimalPlace = (n) => {
  * @returns {string} - 保留小数的字符
  */
 const tostr = (num, decimalLen) => {
-    let isNegtive = num < 0n;
-    let sign = isNegtive ? '-' : '';
-    let result = num.toString();
-    result = result.replace(/\-/, '');
-    const lens = result.length;
-    const inteLens = lens - decimalLen;
-    const inte = inteLens >0 ? result.slice(0, inteLens) : '0';
-    let deci = inteLens >= 0 ? result.slice(inteLens, lens) : result.padStart(decimalLen, '0');
-    deci = deci.replace(/0*$/, '');//这里为了处理多个0结尾的问题，例如5.0000 => 5, 3.30 => 3.3
-    if(deci) return `${sign}${inte}.${deci}`; 
-    return `${sign}${inte}`;
+  let isNegtive = num < 0n;
+  let sign = isNegtive ? '-' : '';
+  let result = num.toString();
+  result = result.replace(/\-/, '');
+  const lens = result.length;
+  const inteLens = lens - decimalLen;
+  const inte = inteLens > 0 ? result.slice(0, inteLens) : '0';
+  let deci = inteLens >= 0 ? result.slice(inteLens, lens) : result.padStart(decimalLen, '0');
+  deci = deci.replace(/0*$/, ''); //这里为了处理多个0结尾的问题，例如5.0000 => 5, 3.30 => 3.3
+  if (deci) return `${sign}${inte}.${deci}`;
+  return `${sign}${inte}`;
 }
 
 /**
@@ -70,18 +70,21 @@ const tostr = (num, decimalLen) => {
  * // returns new SuperDecimal(110000n);
  */
 const assembleNumber = (num, decimalLen) => {
-    if(decimalLen === num.decimalLen) return num; //只对小数位数少的进行更新
-    const { inteLens, bigint } = num;
-    let _num = bigint.toString();
-    _num = _num.padEnd(decimalLen + inteLens, '0');
-    return new SuperDecimal(_num);
+  if (decimalLen === num.decimalLen) return num; //只对小数位数少的进行更新
+  const {
+    inteLens,
+    bigint
+  } = num;
+  let _num = bigint.toString();
+  _num = _num.padEnd(decimalLen + inteLens, '0');
+  return new SuperDecimal(_num);
 }
 
 /** 
  * SuperDecimal类使用JavsScript中的BigInt（ES2017中的一个新的特性），去解决大数字的计算，以及由于浮点数字的标准数字引起的计算精准度的问题{@link http://c.biancheng.net/view/314.html|详情可以参考IEEE-754}
  * */
-class SuperDecimal{
-   
+class SuperDecimal {
+
   /**
    * 用来控制默认的小数点的位数
    * @static
@@ -92,12 +95,12 @@ class SuperDecimal{
    * SuperDecimal类的构造函数
    * @param {(string|number)} numeric - 数字型的参数
    */
-  constructor(numeric){
+  constructor(numeric) {
     const num = toStr(numeric);
-    if(!IsNumeric(num)){
-      throw(new Error(`[input ${numeric}] is not an numerical type, please check it.`))
+    if (!IsNumeric(num)) {
+      throw (new Error(`[input ${numeric}] is not an numerical type, please check it.`))
     }
-    
+
     let [ints, decis] = String(num).split(".").concat("");
     /** 
      * 整数部分
@@ -115,7 +118,7 @@ class SuperDecimal{
      * @type {string}
      * @access public 
      * */
-     this.decimal = decis;
+    this.decimal = decis;
     /** 
      * 小数部分字符长度
      * @type {number}
@@ -137,54 +140,13 @@ class SuperDecimal{
      * @access public */
     this.result = tostr(this.bigint, this.decimalLen);
   }
-  
-  /**
-   * 计算幂函数，这里为了保障结果的精准性，将会尽可能保证小数的位数
-   * @param {number} n - 当前数字的幂函数
-   * @returns {SuperDecimal} - 当前数字的幂函数的结果
-   */
-  pow(n){
-    const _n = new SuperDecimal(n);
-    const decimalLen = n * this.decimalLen;
-    let result = tostr(this.bigint**_n.bigint, decimalLen);
-    return new SuperDecimal(result);
-  }
-  
-  /**
-   * 当前SuperDecimal乘以另一个SuperDecimal
-   * @param {SuperDecimal} base 
-   * @returns {SuperDecimal}
-   */
-  mutiply(base){
-    let result = this.bigint * base.bigint;
-    result = tostr(result, this.decimalLen + base.decimalLen);
-    return new SuperDecimal(result);
-  }
-  
-  /**
-   * 当前SuperDecimal除以另一个SuperDecimal
-   * @param {SuperDecimal} divisor - 除数
-   * @returns {SuperDecimal}
-   */
-  divide(divisor) {
-    const { DIVIDE_DECIMAL_PLACE } = SuperDecimal;
-    const sameDeciaml = Math.max(this.decimalLen, divisor.decimalLen);
-    let up = assembleNumber(this, sameDeciaml);
-    let down = assembleNumber(divisor, sameDeciaml);
-
-    up = up.bigint * decimalPlace(DIVIDE_DECIMAL_PLACE + 2);//这里加2的目的是为了让小数后面的31有效数字，保证在30的数字可以做四舍五入
-    down = down.bigint;
-    let res = (up / down).toString();
-    res = tostr(res, DIVIDE_DECIMAL_PLACE + 1);//保留31数字，为了进行30位有效数字的四舍五入
-    return new SuperDecimal(res);
-  }
 
   /**
    * 两个数字相加
    * @param {SuperDecimal} addtion - 加数
    * @returns {SuperDecimal}
    */
-  add(addtion){
+  add(addtion) {
     const sameDeciaml = Math.max(this.decimalLen, addtion.decimalLen);
     let add1 = assembleNumber(this, sameDeciaml);
     let add2 = assembleNumber(addtion, sameDeciaml);
@@ -199,7 +161,7 @@ class SuperDecimal{
    * @param {SuperDecimal} substraction - 减数
    * @returns {SuperDecimal}
    */
-  sub(substraction){
+  sub(substraction) {
     const sameDeciaml = Math.max(this.decimalLen, substraction.decimalLen);
     let sub1 = assembleNumber(this, sameDeciaml);
     let sub2 = assembleNumber(substraction, sameDeciaml);
@@ -208,13 +170,56 @@ class SuperDecimal{
     res = tostr(res, sameDeciaml);
     return new SuperDecimal(res);
   }
-  
+
+  /**
+   * 当前SuperDecimal乘以另一个SuperDecimal
+   * @param {SuperDecimal} base 
+   * @returns {SuperDecimal}
+   */
+  mutiply(base) {
+    let result = this.bigint * base.bigint;
+    result = tostr(result, this.decimalLen + base.decimalLen);
+    return new SuperDecimal(result);
+  }
+
+  /**
+   * 当前SuperDecimal除以另一个SuperDecimal
+   * @param {SuperDecimal} divisor - 除数
+   * @returns {SuperDecimal}
+   */
+  divide(divisor) {
+    const {
+      DIVIDE_DECIMAL_PLACE
+    } = SuperDecimal;
+    const sameDeciaml = Math.max(this.decimalLen, divisor.decimalLen);
+    let up = assembleNumber(this, sameDeciaml);
+    let down = assembleNumber(divisor, sameDeciaml);
+
+    up = up.bigint * decimalPlace(DIVIDE_DECIMAL_PLACE + 2); //这里加2的目的是为了让小数后面的31有效数字，保证在30的数字可以做四舍五入
+    down = down.bigint;
+    let res = (up / down).toString();
+    res = tostr(res, DIVIDE_DECIMAL_PLACE + 1); //保留31数字，为了进行30位有效数字的四舍五入
+    return new SuperDecimal(res);
+  }
+
+  /**
+   * 计算幂函数，这里为了保障结果的精准性，将会尽可能保证小数的位数
+   * @param {number} n - 当前数字的幂函数
+   * @returns {SuperDecimal} - 当前数字的幂函数的结果
+   */
+  pow(n) {
+    const _n = new SuperDecimal(n);
+    const decimalLen = n * this.decimalLen;
+    let result = tostr(this.bigint ** _n.bigint, decimalLen);
+    return new SuperDecimal(result);
+  }
+
   /**
    * 把当前的SuperDecimal的数字转化成数字的字符串输出
    * @returns {string}
    */
   tostr() {
-      return tostr(this.bigint, this.decimalLen);
+    return tostr(this.bigint, this.decimalLen);
   }
 
   /**
@@ -222,30 +227,35 @@ class SuperDecimal{
    * @param {number} decimal - 保留小数的位数
    * @returns {string}
    */
-  toFixed(decimal = 10){
-    const { result } = this;
-    let  [ints, decis] = String(result).split(".").concat("");
+  toFixed(decimal = 10) {
+    const {
+      result
+    } = this;
+    let [ints, decis] = String(result).split(".").concat("");
     const l = decis.length;
 
-    if(!l){
+    if (!l) { //整数处理方式
       return `${ints}.${''.padEnd(decimal, '0')}`
     }
 
     let res = decis.substr(0, decimal);
     let round = decis.substr(decimal, 1);
-    round = round || 0;
+    round = round || 0; //如果整个位数小于输出的位数
     let newNum = new SuperDecimal(`${ints}.${res}`);
-    if(round >= 5){
-      let one = `0.${'1'.padStart(decimal, '0')}`;
+
+    if (round >= 5) { //四舍五入
+      const sign = this.isNegtive ? '-' : '';
+      let one = `${sign}0.${'1'.padStart(decimal, '0')}`;
       one = new SuperDecimal(one);
       newNum = newNum.add(one);
     }
-    if(newNum.decimalLen){
-      if(decimal <= newNum.decimalLen)
+
+    if (newNum.decimalLen) { //有小数的处理方式
+      if (decimal <= newNum.decimalLen)
         return newNum.result;
       return newNum.result.padEnd(newNum.result.length + decimal - newNum.decimalLen, '0');
     }
-    
+
     return `${newNum.integer}.${''.padEnd(decimal, '0')}`
   }
 
